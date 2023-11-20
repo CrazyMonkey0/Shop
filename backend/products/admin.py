503a56bb1d74c27b5856e05a8115e14f891a1f1a
CrazyMonkey0 <hubcio1765@gmail.com>
2023-11-20 15:23:50 +0100

    Creation of models and administration panel in products application

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    # automatic generation of values
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    # used it in the administrative interface to specify
+    # fields that can be edited directly in the list view
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
