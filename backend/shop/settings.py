a21a9a6b9f496d45414c61fa1e4e99b9a5614009
CrazyMonkey0 <hubcio1765@gmail.com>
2023-11-23 12:55:37 +0100

    Add new app cart

--- backend/shop/settings.py
+++ backend/shop/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'products.apps.ProductsConfig',
-    'orders.apps.OrdersConfig'
+    'orders.apps.OrdersConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Key to store shopping cart in user session
+CART_SESSION_ID = 'cart'
