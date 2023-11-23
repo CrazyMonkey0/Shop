c284b694787efa0fe96ddacbc91608c10f5de3e8
CrazyMonkey0 <hubcio1765@gmail.com>
2023-11-23 16:22:55 +0100

    Creating a view and form of the shopping cart

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -21,7 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls', namespace='products'))
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('', include('products.urls', namespace='products')),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
