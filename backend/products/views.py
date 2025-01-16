83b921ec59ba5b40cf31ea6ec10358bfd47f58fc
CrazyMonkey0 <hubcio1765@gmail.com>
2025-01-17 00:50:21 +0100

    Resolved the issue with subcategories in the store

--- backend/products/views.py
+++ backend/products/views.py
@@ -4,28 +4,21 @@ from cart.forms import CartAddProductForm
 
 
 def product_list(request, category_slug=None):
-    category = None
-    subcategories = None
-    categories = Category.objects.filter(parents__isnull=True)
+    categories = Category.objects.filter(parent__isnull=True) 
     products = Product.objects.filter(available=True)
-
+    category = None
+    
     if category_slug:
-        category = get_object_or_404(
-            Category, slug=category_slug)
-        # Download all subcategories
-        subcategories = category.category_set.all()
-        # Create a list of category IDs (along with the ID of the category itself)
-        category_ids = [category.id] + list(
-            subcategory.id for subcategory in subcategories)
-        # Get all products belonging to the selected categories
-        products = Product.objects.filter(
-            category__id__in=category_ids, available=True)
+        category = get_object_or_404(Category, slug=category_slug)
+        descendant_ids = [descendant.id for descendant in category.get_descendants()]
+        category_ids = [category.id] + descendant_ids
+        products = Product.objects.filter(category__id__in=category_ids, available=True)
 
-    return render(request, 'products/product/list.html',
-                  {'category': category,
-                   'categories': categories,
-                   'products': products,
-                   'subcategories': subcategories, })
+    return render(request, 'products/product/list.html', {
+        'category': category,
+        'categories': categories,
+        'products': products,
+    })
 
 
 def product_detail(request, id, slug):
